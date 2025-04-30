5d1b6562811d96bdc8665901b1618bbfe00eda90
Michel Morais <valentimmichel@gmail.com>
2025-04-30 13:28:45 -0300

    exercicio01 - Vericar número

---
+++ exercicios_parte1/exercicio01/VerificarNumero.java
@@ -0,0 +1,33 @@
+package exercicio01;
+
+import java.util.Scanner;
+
+// Verificar se um número é positivo, negativo ou zero.
+
+public class VerificarNumero {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int numero;
+		do {
+			System.out.print("Informe um númeiro inteiro: ");
+			numero = sc.nextInt();
+
+			if (numero > 0) {
+				System.out.printf("Número %d é POSITIVO!%n", numero);
+			} else if (numero < 0) {
+				System.out.printf("Número %d é NEGATIVO%n", numero);
+			} 
+			System.out.println();
+		} while (numero != 0);
+
+		System.out.printf("O número escolhido foi o %d (Zero)%n", numero);
+		System.out.println();
+		System.out.println("Fim do programa!");
+
+		sc.close();
+
+	}
+
+}
