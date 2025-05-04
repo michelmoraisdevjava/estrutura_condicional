d7ef9cd16055e91858194704c7aedd94dac42468
Michel Morais <valentimmichel@gmail.com>
2025-05-04 15:06:32 -0300

    Sistema Subtrai 5 pontos se o tempo acabar

--- esercicios_parte2/exercicio1/SomarPonto.java
+++ esercicios_parte2/exercicio1/SomarPonto.java
@@ -1,7 +1,7 @@
 package exercicio1;
 
 import java.util.Scanner;
- /*Somar 1 ponto ao jogador se ele acertar a pergunta.*/
+ /*Some 1 ponto ao jogador se ele acertar a pergunta.*/
 public class SomarPonto {
 
 	public static void main(String[] args) {
@@ -9,12 +9,24 @@ public class SomarPonto {
 		
 		int pontos = 0;
 		boolean acertou = false;
+		
 		do {
 		System.out.print("O jogador acertou a pergunta (true/false)? ");
 		acertou = sc.nextBoolean();
 		
 		if(acertou) pontos += 1;
 		}while(acertou);
+		
+		//Subtraia 5 pontos se o tempo acabar
+		final int PENALIZACAO_TEMPO = 5;
+		
+		System.out.print("O tempo acabou? (sim/nao) ");
+		sc.nextLine();
+		String tempoAcabou = sc.nextLine();
+		
+		if(tempoAcabou.equalsIgnoreCase("sim")) {
+			pontos -= PENALIZACAO_TEMPO;
+		}
 		
 		System.out.printf("O jogador acumulou: %d pontos", pontos);
 		
